d807e17e58df9157219f686293bfa3cc3696ea47
Joseph Callaway <jeauxcal@gmail.com>
2016-11-23 09:24:29 -0600

    Fixed responsivness of website load time

--- js/main.js
+++ js/main.js
@@ -131,34 +131,6 @@ $(document).ready(function(){
 	/*	Featured Project Lightbox
 	/* ========================================================================= */
 
-	$(".fancybox").fancybox({
-		padding: 0,
-
-		openEffect : 'elastic',
-		openSpeed  : 650,
-
-		closeEffect : 'elastic',
-		closeSpeed  : 550,
-
-		closeClick : true,
-
-		beforeShow: function () {
-			this.title = $(this.element).attr('title');
-			this.title = '<h3>' + this.title + '</h3>' + '<p>' + $(this.element).parents('.portfolio-item').find('img').attr('alt') + '</p>';
-		},
-
-		helpers : {
-			title : {
-				type: 'inside'
-			},
-			overlay : {
-				css : {
-					'background' : 'rgba(0,0,0,0.8)'
-				}
-			}
-		}
-	});
-
 });
 
 var wow = new WOW ({
